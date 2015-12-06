0f6e65fbb6fd1b078e85e9e3b41c613f4795f751
Abhra Basak <abhra@outlook.in>
2015-12-06 12:58:01 +0530

    Add basic server support

---
+++ Spectator.Server/Server.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Suave;
+
+namespace Spectator.Server {
+    public class Server {
+        public static void Main(string[] args) {
+            Suave.Web.startWebServer(Suave.Web.defaultConfig, Suave.Http.Successful.OK("Hello World"));
+        }
+    }
+}
