b5513872a7d8b54ef5228b9e15b898687cc00826
Abhra Basak <abhra@outlook.in>
2015-12-06 12:59:51 +0530

    Add basic suave hello world server

---
+++ Spectator/Program.fs
@@ -0,0 +1,8 @@
+﻿open Suave
+open Suave.Http.Successful
+open Suave.Web
+
+[<EntryPoint>]
+let main argv = 
+    startWebServer defaultConfig (OK "Hello World!")
+    0 // return an integer exit code
