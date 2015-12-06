1b89fe2779a3e48cd8d55f71e10cc3a6e2328dc4
Abhra Basak <abhra@outlook.in>
2015-12-06 12:55:45 +0530

    Add basic Sass transformer

---
+++ Spectator.SASS/ScssToCssTransformer.cs
@@ -0,0 +1,23 @@
+﻿using LibSassNet;
+using Spectator.Interfaces;
+
+namespace Spectator.SASS {
+    public class ScssToCssTransformer : IStylesheetTransformer {
+        public string Sass { get; set; }
+        public IConfiguration Config { get; set; }
+
+        public ScssToCssTransformer(string sass, IConfiguration config) {
+            this.Sass = sass;
+            this.Config = config;
+        }
+
+        public TransformerResult TransformStylesheet() {
+            var sassCompiler = new SassCompiler();
+            return new TransformerResult { Result = sassCompiler.Compile(this.Sass) };
+        }
+
+        public TransformerResult Transform() {
+            return this.TransformStylesheet();
+        }
+    }
+}
